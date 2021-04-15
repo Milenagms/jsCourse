906325615e3065424228977ece8227819f382b09
Jairo Dias <milenagomes022@gmail.com>
2021-04-15 15:58:01 -0300

    Inicio js

---
+++ BasicJS-ForBeginners/arrayy.js
@@ -0,0 +1,32 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+
+/*  PARA ADICIONAR NO FINAL DO ARRAY 
+alunos.push('luiza');
+alunos.push('Fábio');
+*/
+
+/*  SERVE PARA ALTERAR
+ alunos[0] = 'Milena';
+*/
+
+/*  PARA ADICIONAR NO INICIO DO ARRAY 
+alunos.unshift('Luiza');
+console.log(alunos);
+*/
+
+/* PARA REMOVER DO ARRAY NO FINAL
+const removido = alunos.pop();
+console.log(alunos);
+console.log(removido);
+*/
+
+/* PARA REMOVER DO ARRAY DO INÍCIO
+const removido = alunos.shift();
+console.log(alunos);
+console.log(removido);
+*/
+
+/* PARA FATIAR E PEGAR OS ITENS QUE DETERMINOU
+console.log(alunos.slice(0,-2));
+*/
--- BasicJS-ForBeginners/maisSobreNumeros.js
+++ BasicJS-ForBeginners/maisSobreNumeros.js
@@ -35,3 +35,13 @@ console.log(num1);
 console.log(Number.isInteger(num1));
 
 */
+let num1 = 0.7;
+let num2 = 0.1;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+
+
+num1 = parseFloat(num1.toFixed(2));
+console.log(num1);
+console.log(Number.isInteger(num1));
