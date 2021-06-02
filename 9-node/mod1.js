bb1aa967a0cf0f5d36471c7df6f90719e82e8f09
Milena <milenagomes022@gmail.com>
2021-06-02 15:06:23 -0300

    Node - primeiro contato

---
+++ 9-node/mod1.js
@@ -0,0 +1,15 @@
+const nome = 'Luiz';
+const sobrenome = 'Miranda';
+
+const falaNome = () => {
+    console.log(nome, sobrenome);
+};
+
+// module.exports.nome = nome;
+// module.exports.sobrenome = sobrenome;
+// module.exports.falaNome = falaNome;
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
+this.qualquerCoisa = 'O que eu quiser, mas não se usa muito assim'
